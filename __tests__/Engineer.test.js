c1a40aa5d4370cbe1a9951f954fdac5b1854edf4
Rocket Appliances <monaco.vin@gmail.com>
2022-08-10 09:47:09 -0500

    created prompt and converted to es modules bc require caused error

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,14 @@
 const Engineer = require("../lib/Engineer");
+const bob = new Engineer("bob", 1, "bob@bob.com", "bobGitCode");
 
 test("Engineer class creates an Engineer object with name", () => {
-  const bob = new Engineer("bob", 1, "bob@bob.com");
   expect(bob.name).toBe("bob");
+});
+
+test("getRole returns the correct role", () => {
+  expect(bob.getRole()).toBe("Engineer");
+});
+
+test("getGithub will return user gitHub name", () => {
+  expect(bob.getGitHub()).toBe("bobGitCode");
 });
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
+//const inquirer = require("inquirer");
+import inquirer from "inquirer";
+import Manager from "./lib/Manager";
+//new employees will be pushed to this []
+const employees = [];
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Manager's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the Manager's id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the Manager's email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the Manager's officeNumber?",
+  },
+];
+
+function start() {
+  inquirer.prompt(managerQuestions).then((answers) => {
+    //console.log(answers);
+    const manager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
+    employees.push(manager);
+    console.log(employees);
+  });
+}
+
+start();
+
 /*
-Create employee classes
-Write test for classes
+Create employee classes -
+Write test for classes - couple more tests in other files
 Get information from user - with inquier
 Use information to create employee objects using class I create 
 Add employee to array of employees 
