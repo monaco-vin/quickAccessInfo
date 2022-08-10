a79b323d0d804bdb279ea9ba8361880847d69238
Rocket Appliances <monaco.vin@gmail.com>
2022-08-10 08:27:24 -0500

    project outline and initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+Create employee classes
+Write test for classes
+Get information from user - with inquier
+Use information to create employee objects using class I create 
+Add employee to array of employees 
+Use arroy of employees to create html string
+Use fs to generate html file
+*/
